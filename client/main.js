4a712758dff3ba66cb4a8a8e9d91ffb59a042b2d
victorBM <victorbillardmadrieres@gmail.com>
2017-01-06 11:59:36 +0100

    crud delete - create

--- client/main.js
+++ client/main.js
@@ -3,28 +3,8 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-
-Template.list.helpers({
-  players: function()
-  {
-    // players.insert({ "id":4, "name":"vico" });
-    return players.find();
-  }
-});
-
 Template.posts.helpers({
   list: function(){return Post.find();}
-});
-
-Template.postForm.events({
-  "click button": function(e, t) {
-    var data;
-    data = t.find("#content");
-    Post.insert({
-      content: data.value
-    });
-    return data.value = "";
-  }
 });
 
 Template.post.events({
@@ -34,5 +14,51 @@ Template.post.events({
     return Post.remove({
       _id: post._id
     });
+  }
+});
+
+Template.postForm.events({
+  "click button": function(e, t) {
+    var data;
+    data = t.find("#content");
+    Post.insert({
+      content: data.value
+    });
+    // return data.value = "";
+    FlowRouter.redirect("/list");
+  }
+});
+
+Template.one.events({
+  "click #delete": function(e, t) {
+    var post;
+    post = Post.findOne(t.data);
+    return Post.remove({
+      _id: post._id
+    });
+  }
+});
+
+Template.post.editing = function() {
+  return Session.get("target" + this._id);
+};
+
+Template.post.events({
+  "click #edit": function(e, t) {
+    return Session.set("target" + t.data._id, true);
+  },
+  "keypress input": function(e, t) {
+    var post;
+    if (e.keyCode === 13) {
+      post = Post.findOne(t.data);
+      Post.update({
+        _id: post._id
+      }, {
+        $set: {
+          content: e.currentTarget.value
+        }
+      });
+      return Session.set("target" + t.data._id, false);
+    }
   }
 });
